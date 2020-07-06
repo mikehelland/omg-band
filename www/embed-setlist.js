3646820296792cce46bd25712a04f5b04d11a1a6
mikehelland <mike@mikehelland.com>
2020-07-05 21:54:51 -0700

    viewports

--- www/embed-setlist.js
+++ www/embed-setlist.js
@@ -4,13 +4,7 @@ console.log(data.setCount)
     for (let i = 0; i < data.setCount; i++) {
         let html = "<span style='opacity: 0.6;'>Set " + (i + 1) + ": </span>"
         for (let j = 0; j < data.sets[i].length; j++) {
-            if (j < 5) {
-                html += data.sets[i][j].name + (j < data.sets[i].length - 1 ? ", ": "") 
-            }
-            else if (j === 5) {
-                html += "... more ..."
-            }
-            
+            html += data.sets[i][j].name + (j < data.sets[i].length - 1 ? ", ": "") 
         }
         let el = document.createElement('p')
         el.innerHTML = html
