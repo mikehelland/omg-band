39f7894e27299d1ba5cef3738705441ea1103cfe
mikehelland <mike@mikehelland.com>
2022-10-14 23:35:10 -0700

    don't show deleted shows

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ module.exports = (app, path) => {
     var db = app.get("db")
 
     var find = {}
-    //find["body ->> 'status'"] = "CONFIRMED";
-    //find["body ->> 'promote'"] = true;
     find.status = "CONFIRMED"
     find.promote = true
 
+    find = {}
+    find["body ->> 'type'"] = "GIG";
+    find["body ->> 'status'"] = "CONFIRMED";
+    find["body ->> 'promote'"] = true;
+    find["body ->> 'draft'"]  = null
+    find["deleted"] = "false"
+    
     var options = {order : "body ->> 'when'"};
     
     app.get("/" + path + "promoted", function (req, res) {
 
-        db.things.findDoc(find, options, (err,results) => {
+        db.things.find(find, {}, (err,results) => {
             res.send(err || results)
         })
+        
+        //db.things.findDoc(find, options, (err,results) => {
+        //    res.send(err || results)
+        //})
         
     });
 }
